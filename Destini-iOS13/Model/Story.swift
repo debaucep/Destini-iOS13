76d9defdf86d5f62942ff11387bc75c3395a569d
DmitryShcherba <debaucep@yandex.ru>
2023-02-16 22:16:34 +0300

    Do step3

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+
+}
+
+let example = Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right")
+
+let stories = [
+Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+]
+
